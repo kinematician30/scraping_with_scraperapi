eb41cc5b4f992346b733e86e04fe068d51fe7b69
Kinematician30 <ojoayookuntade@gmail.com>
2024-08-31 09:40:23 +0100

    updated data model in

--- books/books/items.py
+++ books/books/items.py
@@ -8,5 +8,9 @@ import scrapy
 
 class BooksItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    url = scrapy.Field()
+    image = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    prod_desc = scrapy.Field()
     pass
